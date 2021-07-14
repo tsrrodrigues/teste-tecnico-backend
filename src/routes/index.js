0873a3c6b08574b17fc260e1bee70cb9dc6e1bea
tsrrodrigues <tsrrodrigues@outlook.com>
2021-07-14 17:59:43 -0300

    feat: add transaction middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const transactionMiddleware = require('../middlewares/transaction')
+
 const healthRoutes = require('./health')
 const championshipRoutes = require('./championship')
 
 router.use(`/${process.env.API_MEDIPRECO_VERSION}/health`, healthRoutes);
-router.use(`/${process.env.API_MEDIPRECO_VERSION}/championship`, championshipRoutes);
+router.use(`/${process.env.API_MEDIPRECO_VERSION}/championship`, transactionMiddleware, championshipRoutes);
 
 module.exports = router;
